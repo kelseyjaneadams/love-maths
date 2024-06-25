c4dc8a743a52e565690125f523b4e4406c5490c0
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-06-25 16:22:44 +0000

    Fixed division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,8 +38,8 @@ function runGame(gameType) {
     document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() * 10) +1;
-    let num2 = Math.floor(Math.random() * 10) +1;
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -93,6 +93,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     } else if(operator === "-"){
         return[operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+		return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -143,13 +145,23 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    // operand1 === operand2 ? runGame(gameType) : null
-    if (operand1 === operand2){
-        preventDefault()
-        console.log('fish')
-        runGame("addition")
-    }
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "/"; 
+   // Ensure the division results in a whole number
+
+   while (operand1 % operand2 !== 0 || operand1 === operand2) {
+        operand1 = Math.floor(Math.random() * 25) + 1;
+		operand2 = Math.floor(Math.random() * 25) + 1;
+	}
+
+  //  if (operand1 === operand2) {
+	// 	operand1 = Math.floor(Math.random() * 25) + 1;
+	// 	operand2 = Math.floor(Math.random() * 25) + 1;
+	// } 
+
+	// Ensure the larger number is the dividend
+	let dividend = operand1 > operand2 ? operand1 : operand2;
+	let divisor = operand1 > operand2 ? operand2 : operand1;
+
+	document.getElementById("operand1").textContent = dividend;
+	document.getElementById("operand2").textContent = divisor;
+	document.getElementById("operator").textContent = "/";
 }
