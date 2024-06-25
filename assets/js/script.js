6c9d25586ee918edd2015b9605ee15dda450dec4
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-06-25 14:41:24 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // Allow users to press the enter key to submit their answer
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 });
 
@@ -23,6 +30,13 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    // Clears the answer box after each answer
+    document.getElementById("answer-box").value = "";
+
+    // Setting the focus - the cursor to be in the answer box as soon as the page is loaded
+    document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
